26120c0a323e7920dd59731efcafdf80270fff02
lulzx <22330483+Lulzx@users.noreply.github.com>
2018-11-05 23:49:55 +0530

    changes files

--- wifi_e480.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-sudo apt install build-essential git dkms && clear
-git clone https://github.com/tomaspinho/rtl8821ce.git
-cd rtl8821ce
-sudo bash ./dkms-install.sh && sudo modprobe 8821ce
