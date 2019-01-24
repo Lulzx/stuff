494c476fd5c83cb53e06ccbdcc61cb971ec5d687
Lulzx <22330483+Lulzx@users.noreply.github.com>
2019-01-24 18:50:50 +0530

    Update wifi_e480.sh

--- wifi/wifi_e480.sh
+++ wifi/wifi_e480.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install build-essential git dkms && clear
-git clone https://github.com/tomaspinho/rtl8821ce.git
+sudo apt install build-essential git dkms
+git clone --depth=1 https://github.com/tomaspinho/rtl8821ce.git rtl8821ce
 cd rtl8821ce
-sudo bash ./dkms-install.sh && sudo modprobe 8821ce
+sudo bash ./dkms-install.sh
+sudo modprobe 8821ce
