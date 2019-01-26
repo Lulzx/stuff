e66ced0286e19eb6215ddebdf0ca9349f794bb8a
Lulzx <22330483+Lulzx@users.noreply.github.com>
2019-01-27 01:01:39 +0530

    Update arch.sh

--- wifi/arch.sh
+++ wifi/arch.sh
@@ -2,7 +2,7 @@ sudo pacman-mirrors --fasttrack 5 && sudo pacman -Syu
 sudo pacman -S yay git dkms
 git clone --depth=1 https://github.com/tomaspinho/rtl8821ce.git
 cd rtl8821ce
-yay bc rtl8821ce-dkms-git linux-header
-reboot
+yay linux-headers
 sudo bash ./dkms-install.sh
+reboot
 sudo modprobe 8821ce
