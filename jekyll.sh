18367d8f4da417dfa1e955b6f82f6d8232539590
Lulzx <wiserishabh@gmail.com>
2019-07-25 12:15:59 +0530

    Update jekyll.sh

--- jekyll.sh
+++ jekyll.sh
@@ -1,9 +1,11 @@
 sudo apt-get install ruby`ruby -e 'puts RUBY_VERSION[/\d+\.\d+/]'`-dev
 sudo gem install jekyll bundler
 mkdir blog && cd blog
+# add your files now
 bundler init
-nano Gemfile // add gem 'jekyll'
+nano Gemfile # add gem 'jekyll'
 sudo gem install eventmachine -v '1.2.7' --source 'https://rubygems.org/'
 jekyll build
 bundle exec jekyll build
 bundle exec jekyll serve --watch
+# That's all.
