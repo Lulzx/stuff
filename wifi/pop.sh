64ecae4b3cd004d7518d4b8ea945e2c7d32d4a91
lulzx <lulzx@pop-os.localdomain>
2020-04-13 14:02:57 +0530

    clone --depth=1

--- wifi/pop.sh
+++ wifi/pop.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/tomaspinho/rtl8821ce.git
+git clone --depth=1 https://github.com/tomaspinho/rtl8821ce.git
 cd rtl8821ce
 sudo bash ./dkms-install.sh
 sudo modprobe 8821ce
